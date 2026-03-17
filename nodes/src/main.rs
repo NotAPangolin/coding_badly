21085d1c5a763b0b9d6c540b27f3be4549bc7767
Ben McCann <127527327+NotAPangolin@users.noreply.github.com>
2026-03-16 22:28:15 -0500

    nodes changes

--- nodes/src/main.rs
+++ nodes/src/main.rs
@@ -1,13 +1,15 @@
 use macroquad::prelude::*;
 
-pub mod pointManager;
-use crate::pointManager::{Point, PointManager};
+pub mod point_manager;
+use crate::point_manager::{Point, PointManager};
 
 
 #[macroquad::main("Node-ing off")]
 async fn main() {
     // Init Code
     let mut point_mngr = PointManager::new();
+
+    let mut cam_pos: Vec2 = Vec2 { x: 0., y: 0. };
     
 
     // Loops
@@ -15,10 +17,19 @@ async fn main() {
         clear_background(Color { r: 0.8, g: 0.8, b: 0.8, a: 1.0 });
 
         if is_mouse_button_pressed(MouseButton::Left) {
-            point_mngr.add_point(Point::new(mouse_position().0, mouse_position().1, 10.0, SKYBLUE));
+            point_mngr.add_point(Point::new(mouse_position().0+cam_pos.x, mouse_position().1+cam_pos.y, 10.0, Color { r: 0., g: 0.6, b: 0.7, a: 0.8 }));
+        }
+        for key in get_keys_down() {
+            match key {
+                KeyCode::W=>{cam_pos.y -= 1.},
+                KeyCode::A=>{cam_pos.x -= 1.},
+                KeyCode::S=>{cam_pos.y += 1.},
+                KeyCode::D=>{cam_pos.x += 1.},
+                _=>{}
+            }
         }
 
-        point_mngr.render();
+        point_mngr.render(cam_pos);
 
         next_frame().await
     }
--- nodes/src/point_manager.rs
+++ nodes/src/point_manager.rs
@@ -1,16 +1,13 @@
-use std::collections::HashMap;
-
 use macroquad::prelude::*;
 
 pub struct Point {
-    x:f32,
-    y:f32,
+    pos:Vec2,
     r:f32,
     color:Color
 }
 impl Point {
     pub fn new(x:f32, y:f32, r:f32, color:Color) -> Point {
-        Point { x, y, r, color }
+        Point { pos:Vec2::new(x, y), r:r, color:color }
     }
 }
 
@@ -25,9 +22,10 @@ impl PointManager {
     pub fn add_point(&mut self, point:Point) {
         self.points.push( point );
     }
-    pub fn render(&self) {
+    pub fn render(&self, offset:Vec2) {
         for point in self.points.iter() {
-            draw_circle(point.x, point.y, point.r, point.color);
+            let draw_pos = point.pos - offset;
+            draw_circle(draw_pos.x, draw_pos.y, point.r, point.color);
         }
     }
 }
